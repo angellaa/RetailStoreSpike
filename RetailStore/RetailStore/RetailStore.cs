2032be2ff27cb0d24232b904dc32200020833b82
Andrea Angella <angella.andrea@gmail.com>
2016-10-21 16:17:17 +0100

    Add implementation to make the test pass

---
+++ RetailStore/RetailStore/RetailStore.cs
@@ -0,0 +1,17 @@
+﻿namespace RetailStore
+{
+    public class RetailStore
+    {
+        private readonly Screen m_Screen;
+
+        public RetailStore(Screen screen)
+        {
+            m_Screen = screen;
+        }
+
+        public void OnBarcode(string barcode)
+        {
+            m_Screen.ShowText("$12.34");
+        }
+    }
+}
---
+++ RetailStore/RetailStore/Screen.cs
@@ -0,0 +1,9 @@
+﻿namespace RetailStore
+{
+    public class Screen
+    {
+        public void ShowText(string text) => Text = text;
+
+        public string Text { get; set; }
+    }
+}
