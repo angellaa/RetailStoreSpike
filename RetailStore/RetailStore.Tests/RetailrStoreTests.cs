b969f1f73122a844a09eea7a237a980062e98a8b
Andrea Angella <angella.andrea@gmail.com>
2016-10-21 09:29:41 +0100

    Initial thing
    I am confused... not sure how to start

---
+++ RetailStore/RetailStore.Tests/RetailrStoreTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace RetailStore.Tests
+{
+    public class RetailStoreTests
+    {
+        [Test]
+        public void EmptyScreen()
+        {
+            var screen = Mock.Of<IScreen>(x => x.Text == "");
+            new RetailStore(screen);
+
+            Assert.That(screen.Text, Is.EqualTo(""));
+        }
+    }
+
+    public interface IScreen
+    {
+        string Text { get; }
+    }
+
+    public class RetailStore
+    {
+        public RetailStore(IScreen screen)
+        {            
+        }
+    }
+}
